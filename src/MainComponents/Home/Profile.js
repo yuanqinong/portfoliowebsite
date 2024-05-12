9f780164922781559bd526db5ebc7817de02e723
yuanqinong <yuanqin1108@gmail.com>
2024-05-12 22:26:03 +0800

    Update Profile Section

--- src/MainComponents/Home/Profile.js
+++ src/MainComponents/Home/Profile.js
@@ -8,53 +8,58 @@ function Profile(props) {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="https://www.linkedin.com/in/ong-yuan-qin/" target="_blank">
-              <i class="bi bi-linkedin"></i>
-            </a>
-            <a href="https://wa.me/60137698311" target="_blank">
-              <i class="bi bi-whatsapp"></i>
-            </a>
-            <a href="https://github.com/yqfffs" target="_blank">
-              <i class="bi bi-github"></i>
-            </a>
+            <div className="colz-icon">
+              <a href="https://www.linkedin.com/in/ong-yuan-qin/" target="_blank" rel="noreferrer">
+                <i className="bi bi-linkedin"></i>
+              </a>
+              <a href="https://wa.me/60137698311" target="_blank" rel="noreferrer">
+                <i className="bi bi-whatsapp"></i>
+              </a>
+              <a href="https://github.com/yqfffs" target="_blank" rel="noreferrer">
+                <i className="bi bi-github"></i>
+              </a>
+            </div>
           </div>
-
           <div className="profile-details-name">
-            {" "}
             Hello, My Name is <span className="highlighted-text">Yuan Qin</span>
           </div>
           <div className="profile-details-role">
             <span className="primary-text">
-              {" "}
-              <h1>
+              <h1 className="type-animation">
                 <TypeAnimation
                   sequence={[
-                    // Same substring at the start will only be typed out once, initially
-                    "AI Engineer🤖",
-                    1000, // wait 1s before replacing "Mice" with "Hamsters"
-                    "Web Developer💻",
+                    "AI Engineer📚",
                     1000,
-                    "Enthusiastic Developer🤩",
+                    "NLP Engineer✨",
+                    1000,
+                    "CV Engineer🔍",
+                    1000,
+                    "Web Developer🌐",
                     1000,
                   ]}
                   wrapper="span"
                   speed={30}
-                  style={{ fontSize: "1em", display: "inline-block" }}
+                  style={{ fontSize: "40px", fontFamily: "Cursive"}}
                   repeat={Infinity}
                 />
               </h1>
-              <span className="profile-role-tagline">
-                Empowering Tomorrow's World with AI Innovations.
-              </span>
+              <div className="tagline-container">
+                <span className="profile-role-tagline">
+                  Empowering Tomorrow's World with AI Innovations.
+                </span>
+              </div>
             </span>
           </div>
           <div className="profile-options">
-            <button className="btn btn-primary">Hire Me</button>
+            <button className="btn primary-btn">Hire Me</button>
             <a href="Resume_OngYuanQin.pdf" download="Resume_OngYuanQin.pdf">
-            <button className="btn btn-primary">Download Resume</button>
+              <button className="btn highlighted-btn">Get Resume</button>
             </a>
           </div>
         </div>
+        <div className="profile-image">
+          <div className="profile-image-bg"></div>
+        </div>       
       </div>
     </div>
   );
